73682a38078c69f246b8d751a5fd01eae5256d72
Eko Hariyono <56703692+echou11n@users.noreply.github.com>
2019-10-22 18:43:32 +0700

    Eko Hariyono
    Tugas Java menulis sesuai KTP

---
+++ Eko/src/eko/Eko.java
@@ -0,0 +1,42 @@
+public class Eko {
+    public static void main(String[] a) {
+        System.out.println("=== DATA DIRI PRIBADI ===");
+        System.out.println("=========== ===== ===========");
+        System.out.println("=========== ===== ===========");
+    
+        long nik= 3514210907900002L;
+        String nama= " EKO HARIYONO";
+        String tempat= " Pasuruan,";
+        String tgl= " 09 Juli 1990";
+        String jk= " Laki - Laki";
+        String dusun= " Tegalan ";
+        String rt= "Rt 02 ";
+        String rw= "Rw 04";
+        String desa= " Sebalong";
+        String kec= " Nguling";
+        String kab= " Pasuruan";
+        String agama= " Islam";
+        String stkw= " Kawin";
+        String pekerjaan= " Tenaga Pendidik";
+        String kwg= " Warga Negara Indonesia";
+        String jurusan= " Teknik Informatika";
+        long nim= 201969040050L;
+        String angk= "2019 / 2020";
+        
+        System.out.println("NIK : " +nik);
+        System.out.println("Nama :" +nama);
+        System.out.println("Tempat Tanggal Lahir : " +tempat +tgl);
+        System.out.println("Jenis Kelamin :" +jk);
+        System.out.println("Alamat :" +dusun +rt +rw +desa +kec +kab);
+        System.out.println("Agama :" +agama);
+        System.out.println("Status Perkawinan :" +stkw);
+        System.out.println("Pekerjaan :" +pekerjaan);
+        System.out.println("Kewarganegaraan :" +kwg);
+        System.out.println("====================================");
+        System.out.println("Jurusan :" +jurusan);
+        System.out.println("NIM : " +nim);
+        System.out.println("Angkatan :" +angk);
+        System.out.println("====================================");
+    }
+    
+}
